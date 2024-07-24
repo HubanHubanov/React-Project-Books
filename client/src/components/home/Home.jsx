4958d60b5503ce52424e7511f1b2a9e000e584f3
HubanHubanov <huban.hubanov@gmail.com>
2024-07-24 22:31:44 +0300

    Ad home component

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <>
+      <header className="w3-container w3-red w3-center" style={{padding:128 }}>
+        <h1 className="w3-margin w3-jumbo">Welcome to the Best Place for Books</h1>
+        <p className="w3-xlarge">Read all the  Comments and Decide if this will be your Next Book to Read</p>
+        <button className="w3-button w3-black w3-padding-large w3-large w3-margin-top">All books</button>
+      </header>
+      </>
+
+    );
+}
+

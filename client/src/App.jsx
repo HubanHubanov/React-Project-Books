fe0a91cab326353709b3e115e02d8f82f762edca
HubanHubanov <huban.hubanov@gmail.com>
2024-07-24 22:37:55 +0300

    Add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 
@@ -9,6 +10,7 @@ function App() {
     <>
    <Header/>
    <Home/>
+   <Footer/>
       
     </>
   )
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import styles from "../footer/Footer.module.css"
+
+export default function Footer() {
+    return (
+        <footer className="w3-container w3-padding-64 w3-center w3-opacity" > 
+            <div className="w3-xlarge w3-padding-32">
+                <p>© 2024</p>
+                <p>All rights reserved</p>
+                <p>Page Created by Huban Hubanov</p>
+                    <a href="https://softuni.bg" target="_blank">SoftUni</a>
+            </div>
+        </footer>
+    );
+}
+
+<div>© 2020
+<a href="https://softuni.bg">SoftUni</a>
+<p>Created by Huban Hubanov</p>
+</div>

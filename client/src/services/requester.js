d2743c1164205e22970a696e4bc60ef81f2f6bda
HubanHubanov <huban.hubanov@gmail.com>
2024-08-03 12:53:51 +0300

    Add persisted book data in the server

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -19,9 +19,16 @@ async function requester(method, url, data) {
     
    const response = await fetch(url, options);
 
+   
+//    if(response.status == 403) {
+//        localStorage.clear();
+    
+//     }   
+
    if(response.status === 204) {
         return;
    }     
+
 
    const result = await response.json();
 
